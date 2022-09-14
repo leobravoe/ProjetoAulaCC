a5d2f12479905891da391aaf05b08297c0f93ee0
leobravoe <leonardo.bravo.estacio@gmail.com>
2022-09-14 11:41:01 -0300

    Aula 9 e 10

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Produto;
+
+class ProdutoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $produtos = DB::select('SELECT Produtos.id,
+                                       Produtos.nome,
+                                       Produtos.preco,
+                                       Tipo_Produtos.descricao,
+                                       Produtos.ingredientes,
+                                       Produtos.urlImage,
+                                       Produtos.updated_at,
+                                       Produtos.created_at
+                                FROM Produtos
+                                JOIN TIPO_PRODUTOS on Produtos.Tipo_Produtos_id = Tipo_Produtos.id');
+        return view("Produto/index")->with("produtos", $produtos);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $tipoProdutos = DB::select("SELECT * FROM Tipo_Produtos");
+        return view("Produto/create")->with("tipoProdutos", $tipoProdutos);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $produto = new Produto();
+        $produto->nome = $request->nome;
+        $produto->preco = $request->preco;
+        $produto->Tipo_Produtos_id = $request->Tipo_Produtos_id;
+        $produto->ingredientes = $request->ingredientes;
+        $produto->urlImage = $request->urlImage;
+        $produto->save();
+        return $this->index();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $produtos = DB::select("SELECT Produtos.id,
+                                       Produtos.nome,
+                                       Produtos.preco,
+                                       Produtos.Tipo_Produtos_id,
+                                       Tipo_Produtos.descricao,
+                                       Produtos.ingredientes,
+                                       Produtos.urlImage,
+                                       Produtos.updated_at,
+                                       Produtos.created_at
+                                FROM Produtos
+                                JOIN TIPO_PRODUTOS on Produtos.Tipo_Produtos_id = Tipo_Produtos.id
+                                WHERE Produtos.id = ?", [$id]);
+        // O DB SELECT sempre retorna um array [obj], [obj, obj, ....] ou []
+
+        //$produto = Produto::find($id); // Retorna um objeto ou null
+        //dd($produto);
+
+        // Mando carregar a view show de Produto, 
+        // criando dentro dela um objeto chamado "produto"
+        // com o conteúdo de $produto que está no controlador
+        if(count($produtos) > 0)
+            return view("Produto/show")->with("produto", $produtos[0]);
+        // TODO: Implementar mensagens de erro.
+        echo "Produto não encontrado";
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('tipoproduto', "App\Http\Controllers\TipoProdutoController@index")->name("tipoproduto.index");
+Route::get('tipoproduto/create', "App\Http\Controllers\TipoProdutoController@create")->name("tipoproduto.create");
+Route::post('tipoproduto', "App\Http\Controllers\TipoProdutoController@store")->name("tipoproduto.store");
+Route::get('tipoproduto/{id}', "App\Http\Controllers\TipoProdutoController@show")->name("tipoproduto.show");
+
+Route::get('produto', "App\Http\Controllers\ProdutoController@index")->name("produto.index");
+Route::get('produto/create', "App\Http\Controllers\ProdutoController@create")->name("produto.create");
+Route::post('produto', "App\Http\Controllers\ProdutoController@store")->name("produto.store");
+Route::get('produto/{id}', "App\Http\Controllers\ProdutoController@show")->name("produto.show");
+
+
+
+
+
+
+
+// Route::resource('produto', "App\Http\Controllers\ProdutoController");
