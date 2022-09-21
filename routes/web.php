00835f5ed00beb7832e63e4b0118f8a62493e198
leobravoe <leonardo.bravo.estacio@gmail.com>
2022-09-21 11:10:46 -0300

    Aula 11 e 12

--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,28 @@ Route::get('tipoproduto', "App\Http\Controllers\TipoProdutoController@index")->n
 Route::get('tipoproduto/create', "App\Http\Controllers\TipoProdutoController@create")->name("tipoproduto.create");
 Route::post('tipoproduto', "App\Http\Controllers\TipoProdutoController@store")->name("tipoproduto.store");
 Route::get('tipoproduto/{id}', "App\Http\Controllers\TipoProdutoController@show")->name("tipoproduto.show");
+Route::get('tipoproduto/{id}/edit', "App\Http\Controllers\TipoProdutoController@edit")->name("tipoproduto.edit");
 
 Route::get('produto', "App\Http\Controllers\ProdutoController@index")->name("produto.index");
 Route::get('produto/create', "App\Http\Controllers\ProdutoController@create")->name("produto.create");
 Route::post('produto', "App\Http\Controllers\ProdutoController@store")->name("produto.store");
 Route::get('produto/{id}', "App\Http\Controllers\ProdutoController@show")->name("produto.show");
+Route::get('produto/{id}/edit', "App\Http\Controllers\ProdutoController@edit")->name("produto.edit");
+Route::put('produto/{id}', "App\Http\Controllers\ProdutoController@update")->name("produto.update");
 
+
+// use App\Models\Produto;
+// use Illuminate\Support\Facades\DB;
+
+// Route::get('teste', function () {
+//     //$produto = DB::select('SELECT * FROM PRODUTOS where id = 1')[0]; // retorna um array [] ou [obj...]
+//     $produto = Produto::find(1); // retorna null ou obj
+//     //$produto = Produto::where('preco', 8)->first();
+//     //$produto = Produto::where('preco', 8)->get();
+//     //$produto->nome = "Pepperonii";
+//     //$produto->update();
+//     dd($produto);
+// });
 
 
 
